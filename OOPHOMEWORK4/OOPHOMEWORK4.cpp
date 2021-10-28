8a691038762cb8e7d7e307bf184c3f176da6cc62
hellaxv <89920083+hellaxv@users.noreply.github.com>
2021-10-28 22:19:05 +0900

    Update OOPHOMEWORK4.cpp

--- OOPHOMEWORK4/OOPHOMEWORK4.cpp
+++ OOPHOMEWORK4/OOPHOMEWORK4.cpp
@@ -370,6 +370,21 @@ int main()
         obzac2();
         */
     }
+    
+    {
+        obzac();
+        ArrayInt array(6);
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+        array[5] = 6;
+        array.deleteFirst();
+        array.deleteLast();
+        array.print();
+        obzac2();
+    }
 
 
     {  
